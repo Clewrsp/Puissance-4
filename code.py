ce27f4781dea2f50980977b719569947409378cd
22107884anatole <anatole.vallon-hoarau-jooris@ens.uvsq.fr>
2022-04-13 16:18:37 +0200

    On a la grille

--- code.py
+++ code.py
@@ -8,6 +8,8 @@
 import tkinter as tk
 import random as rd
 
+from matplotlib.pyplot import fill
+
 #############################################
 #####################
 # Constantes
@@ -15,10 +17,11 @@ AUCUN = 0
 ROUGE = 1
 JAUNE = 2
 
-LARGEUR = 800
+LARGEUR = 700
 HAUTEUR = 600
 NB_CASE_HAUTEUR = 6
 NB_CASE_LARGEUR = 7
+LARGEUR_CASE = HAUTEUR//NB_CASE_HAUTEUR
 PREMIER_JOUEUR = "ROUGE"
 
 class Jeu():
@@ -38,8 +41,19 @@ class Jeu():
 ###################
 # Fonctions
 
+### Affichage
+def affiche_grille():
+    for i in range(1,NB_CASE_LARGEUR):
+        point = i * LARGEUR_CASE
+        ligne = canvas.create_line((point,0),(point,HAUTEUR),fill="red",width=10)
+    for i in range(1,NB_CASE_LARGEUR-1):
+        point = i * LARGEUR_CASE
+        ligne = canvas.create_line((0,point),(LARGEUR,point),fill="red",width=10)
+        
 
-
+def affiche_cercle():  
+    return
+### Utilitaire
 
 ############################
 # programme principal
@@ -48,5 +62,7 @@ racine = tk.Tk()
 racine.title("La balleuh")
 canvas = tk.Canvas(racine, bg="black", width=LARGEUR, height=HAUTEUR)
 canvas.grid()
+
+affiche_grille()
 
 racine.mainloop()
