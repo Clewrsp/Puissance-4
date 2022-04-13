db7b7b2ee1dd3a340b04020f3f74ea0a34a2147e
Clément <clement.crespin@ens.uvsq.fr>
2022-04-13 17:02:33 +0200

    clem

--- Code C.py
+++ Code C.py
@@ -15,8 +15,8 @@ AUCUN = 0
 ROUGE = 1
 JAUNE = 2
 
-LARGEUR = 700
-HAUTEUR = 600
+LARGEUR = 710
+HAUTEUR = 610
 NB_CASE_HAUTEUR = 6
 NB_CASE_LARGEUR = 7
 LARGEUR_CASE = HAUTEUR//NB_CASE_HAUTEUR
@@ -48,11 +48,25 @@ def affiche_grille():
     for i in range(1,NB_CASE_LARGEUR-1):
         point = i * LARGEUR_CASE
         ligne = canvas.create_line((0,point),(LARGEUR,point),fill="red",width=5)
-
+    canvas.create_line((0,0),(0, LARGEUR), width= 50, fill = "gray")
+    canvas.create_line((HAUTEUR, 0),(HAUTEUR, LARGEUR), width= 5, fill = "white")
 
 def affiche_cercle():
-    canvas.create_oval(LARGEUR_CASE-5,LARGEUR_CASE-5, 0, 0, fill = "blue")
+
+    if event(x,y):
+        canvas.create_oval(LARGEUR_CASE-5,LARGEUR_CASE-5, 0, 0, fill = "blue")
     return
+ 
+def pointeur(event):
+    chaine.configure(text = "Clic détecté en X =" + str(event.x) +\
+                            ", Y =" + str(event.y))
+ 
+    cadre = canvas.create_line(width =200, height =150, bg="light yellow")
+    cadre.bind("<Button-1>", pointeur)
+    cadre.pack()
+    chaine = Label(canvas)
+    chaine.pack()
+ 
 ### Utilitaire
 
 ############################
