deeaf3e34f602fb10eb862742e23841a5109595b
Clément <clement.crespin@ens.uvsq.fr>
2022-04-13 14:28:05 +0200

    chbksjdclnqm

--- code.py
+++ code.py
@@ -1,1 +1,31 @@
+##############################################################
+# Auteurs : Clément Crespin, Anatole Joris, Faustine Passerat
+##############################################################
 
+######################
+# Import des librairies
+
+import tkinter as tk
+import random as rd
+
+#############################################
+#####################
+# Constantes
+
+LARGEUR = 800
+HAUTEUR = 600
+
+#############################################
+###################
+# Fonctions
+
+
+############################
+# programme principal
+
+racine = tk.Tk()
+racine.title("La balleuh")
+canvas = tk.Canvas(racine, bg="black", width=LARGEUR, height=HAUTEUR)
+canvas.grid()
+
+racine.mainloop()
